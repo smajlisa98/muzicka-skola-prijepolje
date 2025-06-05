a4d2cf1503d068d9d758e28d170b6c75c2fef571
jelenap <korugicjelena98@gmail.com>
2025-06-05 15:29:01 +0200

    refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/muzicka-skola-prijepolje/",
+  base: mode === "production" ? "/muzicka-skola-prijepolje/": "/",
   server: {
     host: "::",
     port: 8080,
